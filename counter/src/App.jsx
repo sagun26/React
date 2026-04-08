aba335d2e0cf34b245a94826314ce1697d618013
sagun26 <sagunsigdel6@gmail.com>
2026-04-08 21:13:32 +0545

    counter

---
+++ counter/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  let [counter,setCounter]= useState(0)
+ const addValue =()=>{
+  counter=counter+1;
+  console.log("clicked",counter);
+  setCounter(counter)
+} 
+
+  return (
+    <>
+    
+     <h1> welcome to this page </h1>
+     <h3> counter value:{counter}</h3>
+
+     <button onClick={addValue}>add value{counter}</button>
+    
+     <br/>
+     <button>remove value{counter}</button>
+    <p> footer:{counter}</p>
+    </>
+  )
+}
+
+export default App
