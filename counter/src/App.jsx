6c946b24e6e9e3fd18de7af2de2f392fd508fc20
sagun26 <sagunsigdel6@gmail.com>
2026-04-20 22:19:16 +0545

    updated css

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -18,18 +18,23 @@ function App() {
   setCounter(counter)
 } 
 
+return (
+  <div className="container">
+    <h1>Welcome to this page</h1>
 
-  return (
-    <>
-    
-     <h1> welcome to this page </h1>
-     
-     <button onClick={addValue}>add value{counter}</button>
-    
-     <br/>
-     <button onClick={removeValue}>remove value{counter}</button>
-    </>
-  )
-}
+    <div className="counter-value">{counter}</div>
+
+    {/* Progress Bar */}
+    <div className="progress-container">
+      <div
+        className="progress-bar"
+        style={{ width: `${(counter / 10) * 100}%` }}
+      ></div>
+    </div>
+
+    <button onClick={addValue}>Add Value</button>
+    <button onClick={removeValue}>Remove Value</button>
+  </div>
+)}
 
 export default App
